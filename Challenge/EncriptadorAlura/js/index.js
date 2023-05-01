12174102c7153235b8c1d495166db49d07e321c4
Junior-HJ <112132647+Junior-HJ@users.noreply.github.com>
2023-05-01 17:42:53 -0500

    Version preeliminar del reto

--- Challenge/EncriptadorAlura/js/index.js
+++ Challenge/EncriptadorAlura/js/index.js
@@ -1,5 +1,55 @@
-function copyToClipboard(selector) {
-    const el = document.querySelector(selector);
-    el.select();
+// Encrypt Function
+function encrypt(encrypt = true) {
+    const input = document.getElementById('input-text');
+    const output = document.getElementById('output-text');
+
+    const transformations = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+    if (!validateString(input.value)) {
+        input.value = '';
+        return;
+    }
+
+    let text = input.value;
+    document.getElementById("nothing").classList.add("hidden");
+    document.getElementById("result").classList.remove("hidden");
+    if (encrypt) {
+        for (const [key, value] of Object.entries(transformations)) {
+        text = text.replaceAll(key, value);
+        }
+        alert('Encriptado!');
+    } else {
+        for (const [key, value] of Object.entries(transformations)) {
+        text = text.replaceAll(value, key);
+        }
+        alert('Desencriptado!');
+    }
+    output.value = text;
+}
+
+
+function copyToClipboard() {
+    const inp = document.getElementById('input-text');
+    const out = document.getElementById('output-text');
+    out.select();
     document.execCommand('copy');
+    alert('Copiado!');
+    document.getElementById("nothing").classList.remove("hidden");
+    document.getElementById("result").classList.add("hidden");
+    inp.value = '';
+    out.value = '';
+}
+
+function validateString(texto) {
+    const regex = /^[a-zñ]+$/;
+    if (!regex.test(texto)) {
+        alert('El texto debe tener solo letras minúsculas sin acentos');
+        return false;
+    }
+    return true;
 }
